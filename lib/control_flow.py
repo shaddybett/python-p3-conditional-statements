fea2c03bf622206e2400df932d20fce9f465003b
Shaddy <shadybett540@gmail.com>
2023-11-23 23:02:34 +0300

    solved

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,15 +2,40 @@
 
 def admin_login(username, password):
     # your code here
-    pass
+    if username.lower() == 'admin' and str(password) == '12345':
+        return 'Access granted'
+    else:
+        return 'Access denied' 
+    
+
+result = admin_login('admin','12345')   
 
 def hows_the_weather(temperature):
     # your code here
-    pass
+    if temperature < 40:
+        return "It's brisk out there!"
+    elif 40 < temperature < 65:
+        return "It's a little chilly out there!"
+    elif temperature > 85:
+        return "It's dang hot out there!"
+    else:
+        return "It's perfect out there!"
+    
 
 def fizzbuzz(num):
     # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0 :
+        return "Buzz"
+    else:
+        return num
+    
+ 
+multiple = fizzbuzz(3)
+print(multiple)    
 
 def calculator(operation, num1, num2):
     # your code here
